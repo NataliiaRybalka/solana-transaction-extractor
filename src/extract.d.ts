19dc3d8c6d039f772b074b548e82f0435a6e939a
NataliiaRybalka <natalirybalka21@gmail.com>
2023-02-22 10:25:06 +0200

    interfaces

--- src/extract.d.ts
+++ src/extract.d.ts
@@ -1,111 +1,97 @@
-// export enum blocks {
-//   earliest= 'earliest',
-//   latest= 'latest',
-//   pending= 'pending',
-// }
+export enum source {
+  transaction = 'transaction',
+}
 
-interface UiTokenAmount {
+export enum type {
+  create = 'create',
+  createAccount = 'createAccount',
+  getAccountDataSize = 'getAccountDataSize',
+  initializeAccount3 = 'initializeAccount3',
+  initializeImmutableOwner = 'initializeImmutableOwner',
+  initializeMint2 = 'initializeMint2',
+  mintTo = 'mintTo',
+  mintToChecked = 'mintToChecked',
+  transfer = 'transfer',
+  transferChecked = 'transferChecked',
+}
+
+export enum program {
+  splAssociatedTokenAccount = 'spl-associated-token-account',
+  splToken = 'spl-token',
+  system = 'system',
+}
+
+interface TokenAmount {
   amount: string,
   decimals: number,
   uiAmount: number,
-  uiAmountString: string
+  uiAmountString: string,
 }
 
-interface TokenBalances {
+interface TokenBalance {
   accountIndex: number,
   mint: string,
   owner: string,
   programId: string,
-  uiTokenAmount: UiTokenAmount
+  uiTokenAmount: TokenAmount,
 }
 
 interface Meta {
   computeUnitsConsumed: number,
-  err: boolean,
+  err: boolean | string,////////////////
   fee: number,
   innerInstructions: Array<string>,//////////////////////
   logMessages: Array<string>,
   postBalances: Array<number>,
-  postTokenBalances: Array<TokenBalances>,
+  postTokenBalances: Array<TokenBalance>,
   preBalances: Array<number>,
-  preTokenBalances: Array<TokenBalances>,
+  preTokenBalances: Array<TokenBalance>,
   rewards: Array<string>,/////////////////
-  status: Object
+  status: Object,//////////////////
+}
+
+interface AccountKey {
+  pubkey: string,
+  signer: boolean,
+  source: source,
+  writable: boolean,
+}
+
+interface Info {
+  destination: string,
+  mint: string,
+  multisigAuthority: string,
+  signers: Array<string>,
+  source: string,
+  tokenAmount: TokenAmount,
+}
+
+interface Parsed {
+  info: Info,
+  type: string | type,
+}
+
+interface Instruction {
+  parsed: Parsed,
+  program: program,
+  programId: string,
+}
+
+interface Message {
+  accountKeys: Array<AccountKey>,
+  addressTableLookups: null | Array<string>,
+  instructions: Array<Instruction>,
+  recentBlockhash: string,
 }
 
 export interface Transaction {
+  message: Message,
+  signatures: Array<string>,
+}
+
+export interface TransactionDetail {
   blockTime: number,
   meta: Meta,
-  "slot": 197238273,
-  "transaction": {
-      "message": {
-          "accountKeys": [
-              {
-                  "pubkey": "8D3Dfbs2e7WFBWKvrsEJzZVtTockJLUwBxERi4gorjnN",
-                  "signer": true,
-                  "source": "transaction",
-                  "writable": true
-              },
-              {
-                  "pubkey": "7UstFtoqvzpMGaRrEKSTRuCGRZm6vRpSYp9SpRgDgMJZ",
-                  "signer": false,
-                  "source": "transaction",
-                  "writable": true
-              },
-              {
-                  "pubkey": "CiNZ1qA5StkEn8oeABrr5ip43GRS3PBA8a3C9eMSs858",
-                  "signer": false,
-                  "source": "transaction",
-                  "writable": true
-              },
-              {
-                  "pubkey": "9NWvPxNVqsc7bvk5LRKKjVb3V9xQffwqsXtg3yaJTAkx",
-                  "signer": false,
-                  "source": "transaction",
-                  "writable": false
-              },
-              {
-                  "pubkey": "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr",
-                  "signer": false,
-                  "source": "transaction",
-                  "writable": false
-              },
-              {
-                  "pubkey": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA",
-                  "signer": false,
-                  "source": "transaction",
-                  "writable": false
-              }
-          ],
-          "addressTableLookups": null,
-          "instructions": [
-              {
-                  "parsed": {
-                      "info": {
-                          "destination": "7UstFtoqvzpMGaRrEKSTRuCGRZm6vRpSYp9SpRgDgMJZ",
-                          "mint": "Gh9ZwEmdLJ8DscKNTkTqPbNwLNNBjuSzaG9Vp2KGtKJr",
-                          "multisigAuthority": "8D3Dfbs2e7WFBWKvrsEJzZVtTockJLUwBxERi4gorjnN",
-                          "signers": [
-                              "9NWvPxNVqsc7bvk5LRKKjVb3V9xQffwqsXtg3yaJTAkx"
-                          ],
-                          "source": "CiNZ1qA5StkEn8oeABrr5ip43GRS3PBA8a3C9eMSs858",
-                          "tokenAmount": {
-                              "amount": "10",
-                              "decimals": 6,
-                              "uiAmount": 0.00001,
-                              "uiAmountString": "0.00001"
-                          }
-                      },
-                      "type": "transferChecked"
-                  },
-                  "program": "spl-token",
-                  "programId": "TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA"
-              }
-          ],
-          "recentBlockhash": "GavEACWoAoxN5HU5kCqPNc8bWMUeWsvQdVvcAemvo4XT"
-      },
-      "signatures": [
-          "2QpFBTbWZ2M1hxq3aFQKCUtnd9WoEdz14Xp6ij6zeBK36hrkKZ7nC3L9EHLdTaQthwgoPRDDSu2LtnqPczZzPJDg"
-      ]
-  }
+  slot: number,
+  transaction: Transaction,
 }
